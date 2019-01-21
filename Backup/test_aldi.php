2fcbb2e065f9c44775de99bbd8b937f454010f3e
aldiabdumalik <aldiabdumalik98@gmail.com>
2019-01-21 11:59:38 +0700

    Test_aldi

---
+++ Backup/test_aldi.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test_aldi extends CI_Model {
+
+	
+
+}
+
+/* End of file test_aldi.php */
+/* Location: .//var/www/html/bakmigm/Backup/test_aldi.php */
+?>
